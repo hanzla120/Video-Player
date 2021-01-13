c16b20e40afbad97ba8e9daa27aaab5f07f39e40
hanzla120 <37757501+hanzla120@users.noreply.github.com>
2021-01-13 05:14:07 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,6 +29,27 @@ const cc_btn_style = document.querySelector(".cc_btn_style");
 
 // all functions 
 
+// hide and show controls style 
+function showControlsBarStyle(){
+	progress_bar_container.style.height="5px";
+	progress_bar_play.children[0].style.display = "block";
+}
+function hideControlsBarStyle(){
+	progress_bar_container.style.height="3px";
+	progress_bar_play.children[0].style.display = "none";
+}
+function showControlsStyle(){
+		player_controls.style.opacity = "1";
+		caption_window.style.bottom = "65px";
+		video.style.cursor = "default";
+}
+function hideControlsStyle(){
+		player_controls.style.opacity = "0";
+		caption_window.style.bottom = "12px";
+		video.style.cursor = "none";
+}
+
+
 // roate settings_icon 
 
 // function rotateSettings(){
@@ -52,9 +73,12 @@ function play_pause_func(){
 		
 		video.play();
 		play_pause_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><path class="icon-svg-fill" d="M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z"></path></svg>`;
+		startInactivityTimer();
+		
 	}else{
 		video.pause();
 		play_pause_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><path class="icon-svg-fill" d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"></path></svg>`;
+		showControlsStyle();
 	}
 }
 // ended function 
@@ -105,10 +129,32 @@ function seekTime_func(){
 }
 // get Time 
 function displayTime(time) { //get current time and duration  function
-  const minutes = Math.floor(time / 60);
-  let seconds = Math.floor(time % 60);
-  seconds = seconds > 9 ? seconds : `0${seconds}`;
-  return `${minutes}:${seconds}`;
+	let getMinutes = time / 60;
+	let hours = (getMinutes / 60);
+	let rhours = Math.floor(hours);
+	let minutes = Math.floor((hours - rhours) * 60);
+	let seconds = Math.floor(time % 60);
+		seconds = seconds > 9 ? seconds : `0${seconds}`;
+		minutes = minutes > 9 ? minutes : `0${minutes}`;
+		if(rhours < 1){
+			return `${minutes}:${seconds}`;
+		}else{
+			return `${rhours}:${minutes}:${seconds}`;
+		}
+		
+		
+		
+		
+
+   // minutes = Math.floor(time / 60);
+   // hours = Math.floor(minutes / 60);
+  // let seconds = Math.floor(time % 60);
+  // seconds = seconds > 9 ? seconds : `0${seconds}`;
+   // if(minutes > 60){
+	    // let timeText = `${hours}:${minutes}:${seconds}`;
+	  
+  // }
+  // return `${minutes}:${seconds}`;
 }
 // change progressBar when click
 function progressBarChange(e){
@@ -166,6 +212,8 @@ function volumeDrag(e){
 	video.volume = percent / 100;
 	if(video.volume == 0){
 		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><path class="icon-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z"></path></svg>`;
+	}else if(video.volume < .5){
+		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath ><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path  d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z" transform="translate(0, 0)"></path></clipPath><clipPath ><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path  d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 Z" fill="#fff" ></path><path  d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff"  style="display: none;"></path></svg>`;
 	}else if(video.volume > 0){
 		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z"></path></clipPath><clipPath><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path class="icon-svg-fill icon-svg-volume-animation-speaker" clip-path="url(#icon-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="icon-id-14"></path><path class="icon-svg-fill icon-svg-volume-animation-hider" clip-path="url(#icon-svg-volume-animation-slash-mask)" d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff" id="icon-id-15" style="display: none;"></path></svg>`;
 		
@@ -188,6 +236,8 @@ function volumeBarChange(e){
 	if(video.volume === 0){
 		video.volume = 0;
 		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><path class="icon-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z"></path></svg>`;
+	}else if(video.volume < .5){
+		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath ><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path  d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z" transform="translate(0, 0)"></path></clipPath><clipPath ><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path  d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 Z" fill="#fff" ></path><path  d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff"  style="display: none;"></path></svg>`;
 	}else if(video.volume > 0){
 		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z"></path></clipPath><clipPath><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path class="icon-svg-fill icon-svg-volume-animation-speaker" clip-path="url(#icon-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="icon-id-14"></path><path class="icon-svg-fill icon-svg-volume-animation-hider" clip-path="url(#icon-svg-volume-animation-slash-mask)" d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff" id="icon-id-15" style="display: none;"></path></svg>`;
 		
@@ -461,46 +511,60 @@ video.addEventListener("click", e => {
 		show_controls.classList.remove("panel_height");
 	}
 	});
-	
-// hide cursor and player controls after 5 sec 
 
-(function() {
-    let mouseTimer = null, cursorVisible = true;
 
-    function disappearCursor() {
-        mouseTimer = null;
-        video.style.cursor = "none";
-		player_controls.style.opacity = "0";
-		caption_window.style.bottom = "12px";
-        cursorVisible = false;
-    }
-function mouseMoveFunc() {
-        if (mouseTimer) {
-            window.clearTimeout(mouseTimer);
-        }
-        if (!cursorVisible) {
-            video.style.cursor = "default";
-			player_controls.style.opacity = "1";
-			caption_window.style.bottom = "65px";
-            cursorVisible = true;
-        }
-      if(video.paused == false){
-		 mouseTimer = window.setTimeout(disappearCursor, 3000);
 
+// show controls with onmouseenter
+
+function showControlsEventsFunc(){
+	if(video.paused == false){
+		showControlsStyle();
+		
 	}
-    }
+}
+// hide controls with onmouseleave
+function hideControlsEventsFunc(){
 	if(video.paused){
-		player_controls.style.opacity = "1";
-		caption_window.style.bottom = "65px";
+		showControlsStyle();
+	}else{
+		hideControlsStyle();
+		
 	}
-    video.addEventListener("mousemove",mouseMoveFunc);
+}
+
+// 
+let inactivityTimer;
+const delayTime = 4000;
+function startInactivityTimer(){
+	inactivityTimer = window.setTimeout(doInactivity,delayTime);
+}
+function doInactivity(){
+	if(video.paused == false){
+		hideControlsStyle();
+	}
 	
-	player_controls.onmouseenter = function(){
-		player_controls.style.opacity = "1";
-		caption_window.style.bottom = "65px";
-		window.clearTimeout(mouseTimer);
-	}
-})();
+	
+}
+function resetInactivityTimer(){
+	window.clearTimeout(inactivityTimer);
+	startInactivityTimer();
+	showControlsStyle();
+	
+}
+
+//
+
+// disappear cursor and controls  events
+player_controls.onmouseenter = function(){
+	window.clearTimeout(inactivityTimer);
+};
+progress_bar_container.onmouseenter = showControlsBarStyle;
+progress_bar_container.onmouseleave = hideControlsBarStyle;
+player.onmouseenter = showControlsEventsFunc;
+player.onmouseleave = hideControlsEventsFunc;
+video.onmousemove = resetInactivityTimer;
+
+
 // set volume in localstorage 
 function setVolumeLocalStorage(){
 	localStorage.setItem('videoVolume', video.volume);	
@@ -508,15 +572,19 @@ function setVolumeLocalStorage(){
 }
 // display time and localstorage volume function 
 
-function displayTimeLocalstorageVolumeFunc(){
-		 current_time.textContent = `${displayTime(video.currentTime)}`;
-	duration_time.textContent = `${displayTime(video.duration)}`;
+function localStorageVolumeFunc(){
+	
 	
 	const getLocalstorageVolume = localStorage.getItem('videoVolume');
 	volume_bar.style.width = `${getLocalstorageVolume * 100}%`;
 	video.volume = getLocalstorageVolume;
 	if(video.volume == 0){
 		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><path class="icon-svg-fill" d="m 21.48,17.98 c 0,-1.77 -1.02,-3.29 -2.5,-4.03 v 2.21 l 2.45,2.45 c .03,-0.2 .05,-0.41 .05,-0.63 z m 2.5,0 c 0,.94 -0.2,1.82 -0.54,2.64 l 1.51,1.51 c .66,-1.24 1.03,-2.65 1.03,-4.15 0,-4.28 -2.99,-7.86 -7,-8.76 v 2.05 c 2.89,.86 5,3.54 5,6.71 z M 9.25,8.98 l -1.27,1.26 4.72,4.73 H 7.98 v 6 H 11.98 l 5,5 v -6.73 l 4.25,4.25 c -0.67,.52 -1.42,.93 -2.25,1.18 v 2.06 c 1.38,-0.31 2.63,-0.95 3.69,-1.81 l 2.04,2.05 1.27,-1.27 -9,-9 -7.72,-7.72 z m 7.72,.99 -2.09,2.08 2.09,2.09 V 9.98 z"></path></svg>`;
+	}else if(video.volume < .5){
+		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath ><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path  d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z" transform="translate(0, 0)"></path></clipPath><clipPath ><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path  d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 Z" fill="#fff" ></path><path  d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff"  style="display: none;"></path></svg>`;
+	}else if(video.volume > 0){
+		mute_unmute_btn.innerHTML = `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><defs><clipPath><path d="m 14.35,-0.14 -5.86,5.86 20.73,20.78 5.86,-5.91 z"></path><path d="M 7.07,6.87 -1.11,15.33 19.61,36.11 27.80,27.60 z"></path><path d="M 9.09,5.20 6.47,7.88 26.82,28.77 29.66,25.99 z"></path></clipPath><clipPath><path d="m -11.45,-15.55 -4.44,4.51 20.45,20.94 4.55,-4.66 z" transform="translate(0, 0)"></path></clipPath></defs><path class="icon-svg-fill icon-svg-volume-animation-speaker" clip-path="url(#icon-svg-volume-animation-mask)" d="M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z" fill="#fff" id="icon-id-14"></path><path class="icon-svg-fill icon-svg-volume-animation-hider" clip-path="url(#icon-svg-volume-animation-slash-mask)" d="M 9.25,9 7.98,10.27 24.71,27 l 1.27,-1.27 Z" fill="#fff" id="icon-id-15" style="display: none;"></path></svg>`;
+		
 	}
 }
 
@@ -550,7 +618,15 @@ function cueChangeFunc(e){
 	
 		currentTextTrack.addEventListener("cuechange",cueChangeFunc);
 }
+if(video.textTracks.length != 0){
+customCaptionFunc(video.textTracks[0]);
+	
+caption_btn.style.display = "block";
 
+caption_btn.addEventListener("click",toggleCaption);
+
+
+}
 
 
 
@@ -564,73 +640,44 @@ function toggleCaption(){
 	}
 	
 }
-
-				
-
-if(video.textTracks.length != 0){
-customCaptionFunc(video.textTracks[0]);
-	
-caption_btn.style.display = "block";
-
-caption_btn.addEventListener("click",toggleCaption);
-
-
+// display time 
+function displayCurrentandDuration(){
+	current_time.textContent = `${displayTime(video.currentTime)}`;
+	duration_time.textContent = `${displayTime(video.duration)}`;
 }
+// buffering function 
+function bufferingTrack(){
+	    let percent = null;
+    // FF4+, Chrome
+    if (video && video.buffered && video.buffered.length > 0 && video.buffered.end && video.duration) {
+        percent = video.buffered.end(0) / video.duration;
+    } else if (video && video.bytesTotal != undefined && video.bytesTotal > 0 && video.bufferedBytes != undefined) {
+        percent = video.bufferedBytes / video.bytesTotal;
+    }
 
- video.addEventListener('progress', function() {
-      const bufferedEnd = video.buffered.end(video.buffered.length - 1);
-      const duration =  video.duration;
-      if (duration > 0) {
-		  let percent = ((bufferedEnd / duration)*100);
-		 progress_bar_buffer.style.width = `${percent}%`;
-      }
-    });
-	
-	// video.addEventListener('progress', function(e) {
-    // let percent = null;
-    // if (video && video.buffered && video.buffered.length > 0 && video.buffered.end && video.duration) {
-        // percent = video.buffered.end(0) / video.duration;
-    // } else if (video && video.bytesTotal != undefined && video.bytesTotal > 0 && video.bufferedBytes != undefined) {
-        // percent = video.bufferedBytes / video.bytesTotal;
-    // }
+    if (percent !== null) {
+        percent = 100 * Math.min(1, Math.max(0, percent));
 
-    // if (percent !== null) {
-        // percent = 100 * Math.min(1, Math.max(0, percent));
-
-		// progress_bar_buffer.style.width = `${percent}%`;
+		progress_bar_buffer.style.width = `${percent}%`;
         
 
-    // }
-
-// }, false);
-
-displayTimeLocalstorageVolumeFunc();
-video.onloadeddata = function() {
-	
-  displayTimeLocalstorageVolumeFunc();
-  video.onloadstart = function(){
-	  const loader = document.querySelector(".loader");
-		loader.style.display = "block";
-  }
-	video.onwaiting = function(){
-   const loader = document.querySelector(".loader");
-		loader.style.display = "block";
-	
-};
-
-video.onplaying = function(){
-    const loader = document.querySelector(".loader");
-		loader.style.display = "none";
-};
-video.oncanplay = function(){
-    const loader = document.querySelector(".loader");
-		loader.style.display = "none";
-};
-  
-};
-
-	
+    }
 }
-document.addEventListener("DOMContentLoaded",init_player);
+
+
+// event of buffering track
+video.onprogress = bufferingTrack;
+// events of display current time and duration
+video.onloadedmetadata = displayCurrentandDuration;
+
+
+if(video.duration > 0){
+	displayCurrentandDuration();
+}
+localStorageVolumeFunc();
+}// end init_player
+
+
+window.addEventListener("DOMContentLoaded",init_player);
 // all callback events
 
